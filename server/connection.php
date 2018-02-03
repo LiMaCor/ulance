83b24ca34f48a0e97ee00f9eccda16d809e7b66a
PixelZer0 <pixel_zero@hotmail.com>
2018-02-03 19:24:36 +0100

    Updated connection.php

--- server/connection.php
+++ server/connection.php
@@ -3,19 +3,15 @@
 require 'dbConstants.php';
 
 /**
- * Devuelve 200 si se ha realizado la conexión con la base de datos
+ * Devuelve true o false si se ha realizado la conexión con la base de datos
  */
-
-	$code = null;
 
 	$mysqli = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
 
 	if ($mysqli->connect_errno) {
 		die();
-		$code = 500;
-		return $code;
+		return false;
 	} else {
-		$code = 200;
+		return true;
 	}
-
-	return $code;
+        
