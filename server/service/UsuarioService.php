5305f28e831617d389ca03f32afeab6dc8529f57
PixelZer0 <pixel_zero@hotmail.com>
2018-02-05 00:48:54 +0100

    Updated get() & set() methods from UsuarioService.php

--- server/service/UsuarioService.php
+++ server/service/UsuarioService.php
@@ -6,6 +6,12 @@
  * @author PixelZer0
  */
 
+// CLASES REQUERIDAS
+
+require 'connection.php';
+require 'UsuarioBean.php';
+require 'UsuarioDao.php';
+
 class UsuarioService implements ServiceTableInterface, ServiceViewInterface {
     
     // CONSTRUCTOR
@@ -16,12 +22,43 @@ class UsuarioService implements ServiceTableInterface, ServiceViewInterface {
 
     // MÉTODOS
     
+    private function checkPermission($metodo) {
+        $userSession = $_SESSION['user'];
+        if (isset($userSession)) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
     public function get() {
-        
+        if ($this->checkPermission("get")) {
+            $id = $_GET['id'];
+            $conexion = $mysqli;
+            try {
+                $oBean = new UsuarioBean();
+                $oBean->setId($id);
+                $oDao = new UsuarioDao($conexion);
+                $oBean = $oDao->get($oBean);
+                $json = json_encode($oBean);
+                $oReplyBean = new ReplyBean(200, $json);
+            } catch (Exception $ex) {
+                throw new Exception($ex->getMessage());
+            }
+            return $oReplyBean;
+        } else {
+            return new ReplyBean(401, "Unauthorized operation");
+        }
     }
     
     public function set() {
-        
+        if ($this->checkPermission("set")) {
+            $json = $_GET['json'];
+            $aJson = json_decode($json, true); // Con "true", devuelve un array asociativo
+            // Falta resolver el casteo del array asociativo a una clase personalizada
+        } else {
+            return new ReplyBean(401, "Unauthorized operation");
+        }
     }
     
     public function remove() {
@@ -37,6 +74,14 @@ class UsuarioService implements ServiceTableInterface, ServiceViewInterface {
     }
     
     public function login() {
+        
+    }
+    
+    public function logout() {
+        
+    }
+    
+    public function check() {
         
     }
     
