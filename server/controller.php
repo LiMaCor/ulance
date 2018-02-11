8f42ffb1a863df3f8e0a88bea1453d0f506788d0
PixelZer0 <pixel_zero@hotmail.com>
2018-02-11 20:54:49 +0100

    Updated get object & operation parameters from POST request

--- server/controller.php
+++ server/controller.php
@@ -55,11 +55,13 @@ if (!isset($_SESSION['user'])) {
 $control = new MappingHelper();
 
 $json = json_decode($_POST['json'], true);
+$ob = $_POST['ob'];
+$op = $_POST['op'];
 
 // DEBUGG
 
 //print $json->ob;
 
-$resultado = $control->methodToExecute($json);
+$resultado = $control->methodToExecute($ob, $op, $json);
 
 return '{\"status\":' + $resultado->code + ', \"json\":' + $resultado->json + '}';
