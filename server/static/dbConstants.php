7caca6eff6d09ab51006efcae7a0491046dbd122
PixelZer0 <pixel_zero@hotmail.com>
2018-02-17 11:55:25 +0100

    Updated dbconstants

--- server/static/dbConstants.php
+++ server/static/dbConstants.php
@@ -5,6 +5,6 @@
  */
 
 define("DBUSER", "root");
-define("DBPASS", "bitnami");
+define("DBPASS", "admin");
 define("DBNAME", "ulance");
-define("DBHOST", "127.0.0.1");
+define("DBHOST", "192.168.122.26");
