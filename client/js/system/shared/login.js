68643a31fb0297470fc7bfff08f1bd9ac3e6309c
PixelZer0 <pixel_zero@hotmail.com>
2018-03-25 20:34:19 +0200

    Created 'login.js' file

---
+++ client/js/system/shared/login.js
@@ -0,0 +1,17 @@
+'use strict';
+moduloSistema.controller('LoginController',
+    ['$http', '$scope', '$location', 'constantService', 'sessionServerCallService',
+        function ($http, $scope, $location, constantService, sessionServerCallService) {
+            $scope.title = "Formulario de acceso al sistema";
+            $scope.user = {};
+            $scope.login = function () {
+                sessionServerCallService.login($scope.user.username, $scope.user.password).then(function (response) {
+                    if (response.status == 200) {
+                        $location.path("main");
+                    } else {
+                        return false;
+                    }
+                });
+            };
+        }
+    ]);
