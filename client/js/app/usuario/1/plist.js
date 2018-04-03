3467719653aafa6cd19ff791fad8411e69d19d67
PixelZer0 <pixel_zero@hotmail.com>
2018-04-03 19:51:09 +0200

    Created 'plist.js' file from admin profile in usuario folder

---
+++ client/js/app/usuario/1/plist.js
@@ -0,0 +1,27 @@
+'use strict';
+moduloUsuario.controller('Usuario1PlistController',
+    ['$scope', '$routeParams', '$location', 'serverCallService', 'toolService', 'constantService',
+        function ($scope, $routeParams, $location, serverCallService, toolService, constantService) {
+            $scope.ob = "usuario";
+            $scope.op = "plist";
+            $scope.profile = 1;
+            //-------------------------
+            $scope.np = toolService.checkDefault(1, $routeParams.np);
+            $scope.rpp = toolService.checkDefault(10, $routeParams.rpp);
+            //-------------------------
+            function getDataFromServer() {
+                serverCallService.getCount($scope.ob).then(function (response) {
+                    if (response.status == 200) {
+                        $scope.registers = response.data.json;
+                        $scope.pages = toolService.calculatePages($scope.rpp, $scope.registers)
+                        if ($scope.np > $scope.pages) {
+                            $scope.np = $scope.pages;
+                        }
+                        return serverCallService.getPage($scope.ob, $scope.np, $scope.rpp);
+                    } else {
+                        return false;
+                    }
+                }) // por añadir ".then()"
+            }
+        }
+    ]);
