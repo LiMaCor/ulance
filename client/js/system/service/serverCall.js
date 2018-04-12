4b4459b839b05789057e0e30417b3f678151fd74
PixelZer0 <pixel_zero@hotmail.com>
2018-04-13 01:09:57 +0200

    Established headers by default on all method calls to the server

--- client/js/system/service/serverCall.js
+++ client/js/system/service/serverCall.js
@@ -2,28 +2,29 @@
 moduloServicios.factory('serverCallService',
     ['$http', 'constantService',
         function ($http, constantService) {
+            $http.defaults.headers.put['Content-Type'] = 'application/json;charset=utf-8';
             return {
                 get: function (ob, id) {
                     var data = { ob: ob, op: "get", json: { id: id } };
                     return $http({
-                        url: constantService.getAppUrl(),
-                        method: "POST",
+                        url: constantService.getAppUrl() + "?XDEBUG_SESSION_START",
+                        method: 'GET',
                         params: data
                     })
                 },
                 getCount: function (ob) {
                     var data = { ob: ob, op: "getCount" };
                     return $http({
-                        url: constantService.getAppUrl(),
-                        method: "POST",
-                        params: data
+                        url: constantService.getAppUrl() + "?XDEBUG_SESSION_START",
+                        method: 'GET',
+                        params: data                        
                     })
                 },
                 getPage: function (ob, np, rpp) {
                     var data = { ob: ob, op: "getPage", json: { np: np, rpp: rpp } };
                     return $http({
-                        url: constantService.getAppUrl(),
-                        method: "POST",
+                        url: constantService.getAppUrl() + "?XDEBUG_SESSION_START",
+                        method: 'GET',
                         params: data
                     })
                 }
--- client/js/system/service/sessionServerCall.js
+++ client/js/system/service/sessionServerCall.js
@@ -2,31 +2,30 @@
 moduloServicios.factory("sessionServerCallService",
     ["$http", "constantService",
         function ($http, constantService) {
+            $http.defaults.headers.put['Content-Type'] = 'application/json;charset=utf-8';
             return {
                 login: function (username, password) {
                     password = forge_sha256(password);
                     var data = { ob: "usuario", op: "login", json: { login: username, pass: password } };
-                    // return $http.post(constantService.getAppUrl() + "?XDEBUG_SESSION_START", data);
                     return $http({
                         url: constantService.getAppUrl() + "?XDEBUG_SESSION_START",
                         method: 'GET',
-                        params: data,
-                        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                        params: data
                     })
                 },
                 logout: function () {
                     var data = { ob: "usuario", op: "logout" };
                     return $http({
-                        url: constantService.getAppUrl(),
-                        method: "POST",
+                        url: constantService.getAppUrl() + "?XDEBUG_SESSION_START",
+                        method: 'GET',
                         params: data
                     })
                 },
                 checkSession: function () {
                     var data = { ob: "usuario", op: "getSessionStatus" };
                     return $http({
-                        url: constantService.getAppUrl(),
-                        method: "POST",
+                        url: constantService.getAppUrl() + "?XDEBUG_SESSION_START",
+                        method: 'GET',
                         params: data
                     })
                 }
