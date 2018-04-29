685af67ef2ef086ad764fdeab61b4e5e4556e43f
PixelZer0 <pixel_zero@hotmail.com>
2018-04-29 19:52:06 +0200

    Updated structure and methods

--- client/js/system/shared/main.js
+++ client/js/system/shared/main.js
@@ -1,15 +1,51 @@
 'use strict'
 moduloSistema.controller('MainController',
-    ['$http', '$scope', '$location', 'constantService', 'sessionServerCallService',
-        function ($http, $scope, $location, constantService, sessionServerCallService) {
-            $scope.registros = function () {
+    ['$http', '$scope', '$location', 'constantService', 'sessionServerCallService', 'serverCallService', 'toolService',
+        function ($http, $scope, $location, constantService, sessionServerCallService, serverCallService, toolService) {
+            $scope.ob_cuentabancaria = "cuentabancaria";
+            $scope.ob_movimiento = "movimiento";
+            //-------------------
+            $scope.numeroPagina = 1;
+            $scope.registrosPorPagina = 100;
+            //-------------------
+            $scope.gastos = 0;
+            $scope.ingresos = 0;
+            //-------------------
+            function getDataFromServer() {
                 sessionServerCallService.checkSession().then(function (response) {
                     if (response.data.status == 200) {
-                        $location.path("/registros");
+                        $scope.idUsuario = response.data.json.id;
+                        $scope.filtros = { id_1: $scope.idUsuario };
+                        return serverCallService.getPage($scope.ob_cuentabancaria, $scope.numeroPagina, $scope.registrosPorPagina, $scope.filtros);
+                    } else {
+                        return false;
+                    }
+                }).then(function (response) {
+                    if (response.data.status == 200) {
+                        $scope.cuentas = response.data.json;
+                        angular.forEach($scope.cuentas, function (valor, indice) {
+                            return serverCallService.getPage($scope.ob_movimiento, $scope.numeroPagina, $scope.registrosPorPagina, valor.id).then(function (response) {
+                                if (response.data.status == 200) {
+                                    if (response.data.json.cantidad < 0) {
+                                        $scope.gastos += response.data.json.cantidad;
+                                    } else {
+                                        $scope.ingresos += response.data.json.cantidad;
+                                    }
+                                } else {
+                                    return false;
+                                }
+                            });
+                        });
                     } else {
                         return false;
                     }
                 });
+            }
+            function chartMain() {
+                toolService.chartJS();
+            };
+            $scope.registros = function () {
+                $location.path("/registros");
             };
             $scope.logout = function () {
                 sessionServerCallService.logout().then(function (response) {
@@ -19,6 +55,8 @@ moduloSistema.controller('MainController',
                         return false;
                     }
                 })
-            }
+            };
+            getDataFromServer();
+            chartMain();
         }
     ]);
