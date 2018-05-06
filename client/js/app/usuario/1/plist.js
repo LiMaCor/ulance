a47cb4451e22bfc702092e4f8e751445dc806813
PixelZer0 <pixel_zero@hotmail.com>
2018-05-06 21:09:59 +0200

    Created 'plist' view and controller of usuario

--- client/js/app/usuario/1/plist.js
+++ client/js/app/usuario/1/plist.js
@@ -1,34 +1,51 @@
 'use strict';
-moduloUsuario.controller('Usuario1PlistController',
-    ['$scope', '$routeParams', '$location', 'serverCallService', 'toolService', 'constantService',
-        function ($scope, $routeParams, $location, serverCallService, toolService, constantService) {
+moduloUsuario.controller('Usuarios1Controller',
+    ['$scope', '$location', 'serverCallService', 'sessionServerCallService', 'toolService', 'constantService',
+        function ($scope, $location, serverCallService, sessionServerCallService, toolService, constantService) {
             $scope.ob = "usuario";
-            $scope.op = "plist";
-            $scope.profile = 1;
             //-------------------------
-            $scope.np = toolService.checkDefault(1, $routeParams.np);
-            $scope.rpp = toolService.checkDefault(10, $routeParams.rpp);
+            $scope.numeroPagina = 1;
+            $scope.registrosPorPagina = 10;
             //-------------------------
             function getDataFromServer() {
-                serverCallService.getCount($scope.ob).then(function (response) {
-                    if (response.status == 200) {
-                        $scope.registers = response.data.json;
-                        $scope.pages = toolService.calculatePages($scope.rpp, $scope.registers)
-                        if ($scope.np > $scope.pages) {
-                            $scope.np = $scope.pages;
-                        }
-                        return serverCallService.getPage($scope.ob, $scope.np, $scope.rpp);
+                sessionServerCallService.checkSession().then(function (response) {
+                    if (response.data.status == 200) {
+                        $scope.usuarioEnSesion = response.data.json;
+                        return serverCallService.getCount($scope.ob);
                     } else {
                         return false;
                     }
                 }).then(function (response) {
-                    if (response.status == 200) {
-                        // Añadir "$scope" para mostrar los datos en la vista
+                    if (response.data.status == 200) {
+                        $scope.registros = response.data.json;
+                        $scope.paginas = toolService.calculatePages($scope.registrosPorPagina, $scope.registros)
+                        if ($scope.numeroPagina > $scope.paginas) {
+                            $scope.numeroPagina = $scope.paginas;
+                        }
+                        return serverCallService.getPage($scope.ob, $scope.numeroPagina, $scope.registrosPorPagina);
+                    } else {
+                        return false;
+                    }
+                }).then(function (response) {
+                    if (response.data.status == 200) {
+                        $scope.pagina = response.data.json;
                     } else {
                         return false;
                     }
                 });
             }
+            $scope.logout = function () {
+                sessionServerCallService.logout().then(function (response) {
+                    if (response.data.status == 200) {
+                        $location.path("/login");
+                    } else {
+                        return false;
+                    }
+                });
+            }
+            $scope.anyadirUsuario = function () {
+                $location.path("/usuario/add");
+            };
             getDataFromServer();
         }
     ]);
