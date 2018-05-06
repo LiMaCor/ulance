ccff468513bfa8cb264964c7df1f15e0c299ae8d
PixelZer0 <pixel_zero@hotmail.com>
2018-05-06 21:12:32 +0200

    Fixed useless method

--- client/js/system/service/tool.js
+++ client/js/system/service/tool.js
@@ -25,25 +25,17 @@ moduloServicios.factory('toolService', [function () {
             var myChart = new Chart(ctx, {
                 type: 'pie',
                 data: {
-                    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                    labels: ["Ingresos", "Gastos"],
                     datasets: [{
-                        label: '# of Votes',
-                        data: [12, 50, 22, 18, 35, 31],
+                        label: 'Ingresos y gastos',
+                        data: [50, 22],
                         backgroundColor: [
-                            'rgba(255, 99, 132, 1)',
                             'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
+                            'rgba(255, 99, 132, 1)'
                         ],
                         borderColor: [
-                            'rgba(255,99,132,1)',
                             'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
+                            'rgba(255,99,132,1)'
                         ],
                         borderWidth: 1
                     }]
