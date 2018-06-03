c66a42f1cbea403a95596785eb0d9472a4cee79e
PixelZer0 <pixelzero@gmail.com>
2018-06-03 22:51:38 +0200

    Updated 'profile' view and controller

--- client/js/system/shared/profile.js
+++ client/js/system/shared/profile.js
@@ -3,11 +3,16 @@ moduloSistema.controller('ProfileController',
     ['$http', '$scope', '$location', 'constantService', 'sessionServerCallService', 'serverCallService',
         function ($http, $scope, $location, constantService, sessionServerCallService, serverCallService) {
             $scope.ob = "usuario";
+            $scope.subir = {};
+            //----------------
+            $scope.status = null;
             //----------------
             function getDataFromServer() {
                 sessionServerCallService.checkSession().then(function (response) {
                     if (response.data.status == 200) {
                         $scope.idUsuario = response.data.json.id;
+                        $scope.imgUsuario = response.data.json.imagen;
+                        $scope.isUserImageNull = response.data.json.imagen;
                         return serverCallService.get($scope.ob, $scope.idUsuario);
                     } else {
                         return false;
@@ -26,6 +31,15 @@ moduloSistema.controller('ProfileController',
                         $location.path("/login");
                     } else {
                         return false;
+                    }
+                });
+            }
+            $scope.submit = function () {
+                serverCallService.uploadImg($scope.ob, $scope.subir, $scope.isUserImageNull).then(function (response) {
+                    if (response.data.status == 200) {
+                        $scope.status = "La imagen se ha subido correctamente";
+                    } else {
+                        $scope.status = "Error en la transmisión de datos";
                     }
                 });
             }
