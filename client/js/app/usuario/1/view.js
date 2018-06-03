8487b75fdf3ce01fd4bfa82202f33d6c08a47c4a
PixelZer0 <pixelzero@gmail.com>
2018-06-03 22:49:45 +0200

    Updated 'view' template and controller for Usuario

--- client/js/app/usuario/1/view.js
+++ client/js/app/usuario/1/view.js
@@ -1,14 +1,26 @@
 'use strict'
 moduloUsuario.controller('UsuariosViewController',
-    ['$scope', '$routeParams', '$location', 'serverCallService', 'constantService',
-        function ($scope, $routeParams, $location, serverCallService, constantService) {
+    ['$scope', '$routeParams', '$location', 'serverCallService', 'sessionServerCallService', 'constantService',
+        function ($scope, $routeParams, $location, serverCallService, sessionServerCallService, constantService) {
             $scope.ob = "usuario";
             $scope.idUsuario = $routeParams.id;
+            $scope.subir = {};
+            //----------------
+            $scope.status = null;
             //----------------
             function getDataFromServer() {
-                serverCallService.get($scope.ob, $scope.idUsuario).then(function (response) {
+                sessionServerCallService.checkSession().then(function (response) {
+                    if (response.data.status == 200) {
+                        $scope.imgUsuario = response.data.json.imagen;
+                        $scope.username = response.data.json.login;
+                        return serverCallService.get($scope.ob, $scope.idUsuario);
+                    } else {
+                        return false;
+                    }
+                }).then(function (response) {
                     if (response.data.status == 200) {
                         $scope.datosUsuario = response.data.json;
+                        $scope.isUserImageNull = response.data.json.imagen;
                     } else {
                         return false;
                     }
@@ -20,6 +32,15 @@ moduloUsuario.controller('UsuariosViewController',
                         $location.path("/login");
                     } else {
                         return false;
+                    }
+                });
+            }
+            $scope.submit = function () {
+                serverCallService.uploadImg($scope.ob, $scope.subir, $scope.isUserImageNull).then(function (response) {
+                    if (response.data.status == 200) {
+                        $scope.status = "La imagen se ha subido correctamente";
+                    } else {
+                        $scope.status = "Error en la transmisión de datos";
                     }
                 });
             }
